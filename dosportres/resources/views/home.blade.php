b025b82994ba25f63717cb7682276eed77e6de47
patricio <patriciocabreravera@gmail.com>
2017-08-30 14:08:00 -0300

    Se creo login y register para acceder al sistema

---
+++ dosportres/resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <button class="btn btn-success">Crear Nuevo Post </button>
+                        </div>
+                            
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
